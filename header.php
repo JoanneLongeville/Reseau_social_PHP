8699d6739dbc5c409208581ab82e517f78eed991
Joanne Longeville <joanne.longeville@gmail.com>
2023-02-27 13:53:13 +0100

    modif logo + noms des pages

--- header.php
+++ header.php
@@ -1,19 +1,19 @@
 <header>
-    <a href='admin.php'><img src="img/resoc.jpg" alt="Logo de notre réseau social"/></a>
+    <a href='admin.php'><img src="img/logo-07.png" alt="Logo de notre réseau social"/></a>
     <nav id="menu">
-        <a href="news.php">Actualités</a>
-        <a href="wall.php">Mur</a>
-        <a href="feed.php">Flux</a>
-        <a href="tags.php">Mots-clés</a>
-        <a href="post.php">Publier</a>
+        <a href="news.php">News</a>
+        <a href="wall.php">Wall</a>
+        <a href="feed.php">Feed</a>
+        <a href="tags.php">Hashtags</a>
+        <a href="post.php">Post</a>
     </nav>
     <nav id="user">
         <a href="#">Profil</a>
             <ul>
-                <li><a href="login.php">Connexion / Inscription</a></li>
-                <li><a href="settings.php">Paramètres</a></li>
-                <li><a href="followers.php">Mes suiveurs</a></li>
-                <li><a href="subscriptions.php">Mes abonnements</a></li>
+                <li><a href="login.php">Login / Subscribe</a></li>
+                <li><a href="settings.php">Settings</a></li>
+                <li><a href="followers.php">Followers</a></li>
+                <li><a href="subscriptions.php">Subscriptions</a></li>
                 <?php 
                 if (isset($_SESSION['connected_id'])) { //si il y a une session ouverte...
                 ?> 
