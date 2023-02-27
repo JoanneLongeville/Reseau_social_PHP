1898ad7da7dc10f1cac0e91123a41718cbe0912a
Joanne Longeville <joanne.longeville@gmail.com>
2023-02-27 13:56:49 +0100

    initialisation

--- addLike.php
+++ addLike.php
@@ -12,7 +12,9 @@ if (isset($_SESSION['connected_id'])){
             if(!$ok){
                 echo "L'ajout du like a échoué : " . $mysqli->error;
             }
+            // echo "Cet utilisateur n'a jamais liké.";
         }else {
+            //echo "<article> Vous avez déjà liké ce post.</article>";
             $delete_like_sql = "DELETE FROM socialnetwork.likes WHERE user_id = '$session_id' AND post_id = '$current_post_id'";
             $ok = $mysqli->query($delete_like_sql);
             if (!$ok) {
