a697ff4a3ac9849d5fef1c2bb34c33284cfb44ca
Joanne Longeville <joanne.longeville@gmail.com>
2023-02-28 14:29:45 +0100

    ajout format date

--- wall.php
+++ wall.php
@@ -18,22 +18,22 @@
     <div id="wrapper">
         <aside>
             <?php                   
-                $laQuestionEnSql = "SELECT * FROM users WHERE id= '$userId' ";
-                $lesInformations = $mysqli->query($laQuestionEnSql);
-                $user = $lesInformations->fetch_assoc();
-                $enCoursDeTraitement = isset($_POST['follow']);
-                if ($enCoursDeTraitement) {
-                    $follower = $_SESSION['connected_id'];
-                    $followed = $user["id"];
-                    $instructionSql = "INSERT INTO followers" . "(id, followed_user_id, following_user_id)" . "VALUES (NULL,"
-                        . $followed . ", " . $follower . ");";
-                    $ok = $mysqli->query($instructionSql);
-                    if (!$ok) {
-                        echo "impossible de s'abonner";
-                    } else {
-                        echo "vous etes abonné";
-                    }
+            $laQuestionEnSql = "SELECT * FROM users WHERE id= '$userId' ";
+            $lesInformations = $mysqli->query($laQuestionEnSql);
+            $user = $lesInformations->fetch_assoc();
+            $enCoursDeTraitement = isset($_POST['follow']);
+            if ($enCoursDeTraitement) {
+                $follower = $_SESSION['connected_id'];
+                $followed = $user["id"];
+                $instructionSql = "INSERT INTO followers" . "(id, followed_user_id, following_user_id)" . "VALUES (NULL,"
+                    . $followed . ", " . $follower . ");";
+                $ok = $mysqli->query($instructionSql);
+                if (!$ok) {
+                    echo "impossible de s'abonner";
+                } else {
+                    // echo "vous etes abonné";
                 }
+            }
             ?>                
             <img src="img/user.jpg" alt="Portrait de l'utilisatrice"/>
             <section>
@@ -58,39 +58,39 @@
 
         <main>
             <?php
-                include('addLike.php');
-                $laQuestionEnSql = "
-                    SELECT posts.content, 
-                    posts.id as post_id,
-                    posts.created, 
-                    users.alias as author_name,
-                    users.id, 
-                    COUNT(likes.id) as like_number, 
-                    GROUP_CONCAT(DISTINCT tags.label) AS taglist,
-                    GROUP_CONCAT(DISTINCT tags.id) as tagID 
-                    FROM posts
-                    JOIN users ON  users.id=posts.user_id
-                    LEFT JOIN posts_tags ON posts.id = posts_tags.post_id  
-                    LEFT JOIN tags       ON posts_tags.tag_id  = tags.id 
-                    LEFT JOIN likes      ON likes.post_id  = posts.id 
-                    WHERE posts.user_id='$userId' 
-                    GROUP BY posts.id
-                    ORDER BY posts.created DESC  
-                    ";
-                $lesInformations = $mysqli->query($laQuestionEnSql);
-                if ( ! $lesInformations){
-                    echo("Échec de la requete : " . $mysqli->error);
-                }
-                while ($post = $lesInformations->fetch_assoc()){
+            include 'addLike.php';
+            $laQuestionEnSql = "
+                SELECT posts.content, 
+                posts.id as post_id,
+                posts.created, 
+                users.alias as author_name,
+                users.id, 
+                COUNT(likes.id) as like_number, 
+                GROUP_CONCAT(DISTINCT tags.label) AS taglist,
+                GROUP_CONCAT(DISTINCT tags.id) AS tagID 
+                FROM posts
+                JOIN users ON  users.id = posts.user_id
+                LEFT JOIN posts_tags ON posts.id = posts_tags.post_id  
+                LEFT JOIN tags       ON posts_tags.tag_id  = tags.id 
+                LEFT JOIN likes      ON likes.post_id  = posts.id 
+                WHERE posts.user_id = '$userId' 
+                GROUP BY posts.id
+                ORDER BY posts.created DESC  
+                ";
+            $lesInformations = $mysqli->query($laQuestionEnSql);
+            if ( !$lesInformations){
+                echo("Échec de la requete : " . $mysqli->error);
+            }
+            while ($post = $lesInformations->fetch_assoc()){
             ?>                                   
             <article>
                 <h3>
-                    <time datetime='2020-02-01 11:12:13' ><?php echo $post['created'] ?></time>
+                    <time><?php include 'formatDate.php' ?></time>
                 </h3>
-                <address><?php echo $post['author_name'] ?></address>
+                <address>from <?php echo $post['author_name'] ?></address>
                 <div>
                     <p><?php echo $post['content'] ?></p>
-                </div>                                            
+            </div>                                            
                 <footer>
                     <small>
                         <form method="post">
