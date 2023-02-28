8b130851ae52b0d508d276280ec4a474fb09549a
Joanne Longeville <joanne.longeville@gmail.com>
2023-02-28 11:05:18 +0100

    correction logout

--- header.php
+++ header.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['logout'])){ 
+    session_destroy(); 
+    header('Refresh:0');
+    header('Location: login.php'); 
+}
+?>
+
 <header>
     <a href='admin.php'><img src="img/logo-07.png" alt="Logo de notre réseau social"/></a>
     <nav id="menu">
@@ -14,23 +23,17 @@
                 <li><a href="settings.php">Settings</a></li>
                 <li><a href="followers.php">Followers</a></li>
                 <li><a href="subscriptions.php">Subscriptions</a></li>
+                <li>             
                 <?php 
                 if (isset($_SESSION['connected_id'])) { //si il y a une session ouverte...
                 ?> 
-                <form method="post">
-                    <button type="sumbit" name="logout">Logout</button>
-                </form>
+                <a><form method="post">
+                    <button type="submit" name="logout">Logout</button>
+                </form></a>
                 <?php 
                 } 
                 ?>
+                </li>
             </ul>
     </nav>
 </header>
-
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['logout'])){ 
-    session_destroy(); 
-    //header('Refresh:0');
-    header('Location: login.php'); 
-}
-?>
